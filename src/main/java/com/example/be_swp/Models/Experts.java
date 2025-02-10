b8fecf4b8edaa58f743f940eb79f6fe1085e3aaf
Kuni <binlen11112@gmail.com>
2025-02-10 22:20:26 +0700

    Update Entity Experts and Service

--- src/main/java/com/example/be_swp/Models/Experts.java
+++ src/main/java/com/example/be_swp/Models/Experts.java
@@ -1,22 +1,39 @@
 package com.example.be_swp.Models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Experts {
 
     @Id
-    private int estheticianID;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int expertID;
 
-    private String fullName;
+    private int userID;
 
-    private String specializeIn;
+    private String specialization;
 
     private int yearOfExperiences;
 
-    private String email;
+    private String description;
 
-    public Experts() {
-    }
+    private int status;
+
+    private LocalDateTime created_at;
+
+    private LocalDateTime updated_at;
 
 }
--- src/main/java/com/example/be_swp/Models/Service.java
+++ src/main/java/com/example/be_swp/Models/Service.java
@@ -1,9 +1,19 @@
 package com.example.be_swp.Models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Service {
 
     @Id
@@ -16,10 +26,15 @@ public class Service {
 
     private String description;
 
-    @OneToMany(mappedBy = "service")
-    private List<Appointments> bookingHistories;
+    private int duration;
 
-    public Service() {
-    }
+    private int status;
+
+    private String type;
+
+    private LocalDateTime created_at;
+
+    private LocalDateTime updated_at;
+
 
 }
