8396c4e89d344f738b5392aa6ed622ffe728f880
KhaTran-Git <khatxse1008@gmail.com>
2025-02-16 21:41:52 +0700

    register commit

---
+++ src/main/java/com/example/be_swp/Config/SercurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.be_swp.Config;
+
+import com.example.be_swp.Service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SercurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Tắt CSRF nếu không cần
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui/**","/api/**","/v3/api-docs/**").permitAll() // Cho phép truy cập API mà không cần login
+                        .anyRequest().authenticated() // Các request khác phải login
+                )
+                .formLogin(form -> form.disable()) // Tắt form login mặc định
+                .httpBasic(basic -> basic.disable()); // Tắt Basic Auth
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
