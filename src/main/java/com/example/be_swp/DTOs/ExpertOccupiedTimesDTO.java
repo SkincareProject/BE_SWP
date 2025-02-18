936367190d77baafe03e713f18da1f484cd1c2b5
Kuni <binlen11112@gmail.com>
2025-02-18 12:57:28 +0700

    update dataInit(again), OccupiedTime Controller (findAll and findByExepertId)

---
+++ src/main/java/com/example/be_swp/DTOs/ExpertOccupiedTimesDTO.java
@@ -0,0 +1,37 @@
+package com.example.be_swp.DTOs;
+
+import com.example.be_swp.Models.Experts;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ExpertOccupiedTimesDTO {
+
+    private int expertOccupiedTimeId;
+
+    private int expertId;
+
+    private LocalDate date;
+
+    private LocalTime startAt;
+
+    private LocalTime endAt;
+
+    private int status;
+
+    private LocalDateTime created_at;
+
+    private LocalDateTime updated_at;
+
+}
