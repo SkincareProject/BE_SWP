5840c3e29475abc6e7006817709dd86d5fe0513b
KhaTran-Git <khatxse1008@gmail.com>
2025-03-03 01:08:32 +0700

    user

---
+++ src/main/java/com/example/be_swp/DTOs/test/QuizDTO.java
@@ -0,0 +1,22 @@
+package com.example.be_swp.DTOs.test;
+
+import com.example.be_swp.Models.Questions;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class QuizDTO {
+    private int quizId;
+    private String name;
+    private String expertName;
+    private LocalDateTime createAt;
+    private List<Questions> questions;
+}
+
+
