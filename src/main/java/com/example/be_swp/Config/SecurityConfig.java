417427ee7d04b49b1396e7823dd9c69323f5a769
TamHM1410 <hunhminhtam@gmail.com>
2025-04-05 18:41:46 +0700

    ok

--- src/main/java/com/example/be_swp/Config/SecurityConfig.java
+++ src/main/java/com/example/be_swp/Config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     @Order(0)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors((cors) -> cors.configurationSource(corsConfigurationSource()))
+//                .cors((cors) -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
